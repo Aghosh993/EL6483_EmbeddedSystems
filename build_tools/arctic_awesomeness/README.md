f3ef4881cbc32084898e6770250bdb52561d3dcc
Aghosh993 <Aghosh993@gmail.com>
2017-01-25 07:38:20 -0500

    Adding arctic awesomeness build platform for CLI builds in Linux.

---
+++ build_tools/arctic_awesomeness/README.md
@@ -0,0 +1,4 @@
+STM32CubeF3-based flight software development repo for the STM32 F3 
+Discovery Board.
+
+(c) Abhimanyu Ghosh, 2016
---
+++ build_tools/arctic_awesomeness/blinky/load_fw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+$1/openocd -f ./stm32f3discovery.cfg &
+$2/arm-none-eabi-gdb --batch --command=runme.gdb
+pkill openocd
