692bcd3736e1e12a2ddbc49a3a1e9d3218d08636
Aghosh993 <Aghosh993@gmail.com>
2017-01-26 03:16:47 -0500

    Fixed some sketchy stuff going on in load_fw.

---
+++ build_tools/arctic_awesomeness/f3_basic_app/f3_basic_app.c
@@ -0,0 +1,79 @@
+/*
+	This is the main user software application
+	In this case, we have below the code necessary for a simple blinking LED program that toggles one of the LEDs on the
+	Discovery PCB at 1 Hz.
+
+	(c) Abhimanyu Ghosh, 2016
+ */
+
+#include "stm32f3xx_hal.h"
+
+static void Error_Handler(void)
+{
+	while(1);
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+int main()
+{
+	static GPIO_InitTypeDef  GPIO_InitStruct;
+
+	GPIO_InitStruct.Pin = GPIO_PIN_8;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+	HAL_Init();
+
+	SystemClock_Config();
+
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET);
+
+	
+
+	/*
+		Loop indefinitely:
+	 */
+	while(1)
+	{
+		HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_8);
+		HAL_Delay(150);
+		HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_8);
+		HAL_Delay(150);
+	}
+
+	return 0; // We should never get here...
+}
