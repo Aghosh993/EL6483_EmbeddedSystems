4f4559224fe4df00b39c690573983ddddb39771d
Aghosh993 <Aghosh993@gmail.com>
2017-01-27 02:00:07 -0500

    Updating mkproject script to also build for F4.

--- build_tools/arctic_awesomeness/mkproject.sh
+++ build_tools/arctic_awesomeness/mkproject.sh
@@ -11,16 +11,18 @@ PROJ_PLATFORM=$2
 
 if [ "$2" = "F3" ] || [ "$2" = "f3" ]; then
 	echo "Creating F3 Discovery project..."
+	BASE_PROJ_NAME=f3_basic_app
 else
 	if [ "$2" = "F4" ] || [ "$2" = "f4" ]; then
 		echo "Creating F4 Discovery project..."
+		BASE_PROJ_NAME=f4_basic_app
 	else
 		echo "ERROR! Invalid platform!!"
 		exit 0
 	fi
 fi
 
-cp -r f3_basic_app/ $PROJ_NAME
-mv $PROJ_NAME/f3_basic_app.c $PROJ_NAME/$PROJ_NAME.c
+cp -r $BASE_PROJ_NAME $PROJ_NAME
+mv $PROJ_NAME/$BASE_PROJ_NAME.c $PROJ_NAME/$PROJ_NAME.c
 echo $PROJ_NAME > $PROJ_NAME/proj_name
-sed -i "s/f3_basic_app/$PROJ_NAME/g" $PROJ_NAME/runme.gdb
+sed -i "s/$BASE_PROJ_NAME/$PROJ_NAME/g" $PROJ_NAME/runme.gdb
