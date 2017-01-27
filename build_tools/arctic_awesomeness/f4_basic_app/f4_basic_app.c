9fa529f99b69d7d4686eee1104ef1ca8b4d51269
Aghosh993 <Aghosh993@gmail.com>
2017-01-27 01:06:57 -0500

    Adding F4 workspace.

---
+++ build_tools/arctic_awesomeness/f4_basic_app/f4_basic_app.c
@@ -0,0 +1,16 @@
+/*
+	This is the main user software application
+	In this case, we have below the code necessary for a simple blinking LED program that toggles one of the LEDs on the
+	Discovery PCB at 1 Hz.
+
+	(c) Abhimanyu Ghosh, 2016
+ */
+
+#include "stm32f4xx_hal.h"
+
+int main()
+{
+	while(1);
+
+	return 0;
+}
