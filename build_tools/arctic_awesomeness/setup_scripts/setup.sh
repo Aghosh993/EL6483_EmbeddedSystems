a62b5810d25db32bd21c90d74f493b5ba2d398df
Aghosh993 <Aghosh993@gmail.com>
2017-01-27 01:50:21 -0500

    Updated setup script to build OpenOCD from source.

--- build_tools/arctic_awesomeness/setup_scripts/setup.sh
+++ build_tools/arctic_awesomeness/setup_scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PKG_MGR_CMD=""
-DEPENDENCY_PKGS="openocd"
+DEPENDENCY_PKGS="libtool make pkg-config"
+MAKE_OPTS="-j5"
 
 echo "Obtaining dependencies..."
 
@@ -26,6 +27,12 @@ echo "Downloading and extracting toolchain..."
 wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
 tar xvf gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2 -C ../../../..
 rm gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
+
+echo "Downloading OpenOCD..."
+wget https://superb-dca2.dl.sourceforge.net/project/openocd/openocd/0.10.0/openocd-0.10.0.tar.bz2
+tar xvf openocd-0.10.0.tar.bz2
+cd openocd-0.10.0/ && ./configure --enable-ft2232 --enable-stlink && make $(MAKE_OPTS) && make install && cd ..
+rm -r openocd*
 
 echo "Copying Udev rules to /etc/udev/rules.d/"
 cp *.rules /etc/udev/rules.d/
