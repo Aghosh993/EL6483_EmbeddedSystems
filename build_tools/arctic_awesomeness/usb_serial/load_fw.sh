2e128ca8e1d6a73ae6bb931ce63bb186b76e9715
Aghosh993 <Aghosh993@gmail.com>
2017-02-16 14:17:53 -0500

    Fixing Mac F4 Makefile. Abhi.

--- build_tools/arctic_awesomeness/usb_serial/load_fw.sh
+++ build_tools/arctic_awesomeness/usb_serial/load_fw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openocd -f board/stm32f4discovery.cfg &
+openocd -f ./stm32f3discovery.cfg &
 sleep 2
 $1/arm-none-eabi-gdb --batch --command=runme.gdb 
 echo "Killing OpenOCD..."
---
+++ build_tools/arctic_awesomeness/usb_serial/runme.gdb
@@ -0,0 +1,3 @@
+target remote localhost:3333
+monitor reset halt
+load usb_serial.elf
