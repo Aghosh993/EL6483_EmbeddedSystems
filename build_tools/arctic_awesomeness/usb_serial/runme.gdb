6a69608752d4ffd7e2826c6187303b38f42cbc35
Aghosh993 <Aghosh993@gmail.com>
2017-03-07 22:09:26 -0500

    Removing extra dir.

--- build_tools/arctic_awesomeness/usb_serial/runme.gdb
+++
@@ -1,3 +0,0 @@
-target remote localhost:3333
-monitor reset halt
-load usb_serial.elf
--- build_tools/arctic_awesomeness/usb_serial/user_include/hal_interface/hal_common_includes.h
+++
@@ -1,6 +0,0 @@
-#ifndef HAL_COMMON_INCLUDES_H
-#define HAL_COMMON_INCLUDES_H	1
-
-#include "stm32f3xx_hal.h"
-
-#endif
