dc5d8a33c925fa2763f14bda35664bbf27f53b7b
Aghosh993 <Aghosh993@gmail.com>
2017-03-10 00:25:44 -0500

    Fixing some bugs.

--- build_tools/arctic_awesomeness/arch/stm32/f3/src/stm32f3xx_hal_timebase_tim.c
+++ build_tools/arctic_awesomeness/arch/stm32/f3/src/stm32f3xx_hal_timebase_tim.c
@@ -69,64 +69,64 @@ void TIM6_DAC_IRQHandler(void);
   * @param  TickPriority: Tick interrupt priority.
   * @retval HAL status
   */
-// HAL_StatusTypeDef HAL_InitTick (uint32_t TickPriority)
-// {
-//   RCC_ClkInitTypeDef    clkconfig;
-//   uint32_t              uwTimclock, uwAPB1Prescaler = 0U;
-//   uint32_t              uwPrescalerValue = 0U;
-//   uint32_t              pFLatency;
+HAL_StatusTypeDef HAL_InitTick (uint32_t TickPriority)
+{
+  RCC_ClkInitTypeDef    clkconfig;
+  uint32_t              uwTimclock, uwAPB1Prescaler = 0U;
+  uint32_t              uwPrescalerValue = 0U;
+  uint32_t              pFLatency;
   
-//     /*Configure the TIM6 IRQ priority */
-//   HAL_NVIC_SetPriority(TIM6_DAC_IRQn, TickPriority ,0U);
+    /*Configure the TIM6 IRQ priority */
+  HAL_NVIC_SetPriority(TIM6_DAC_IRQn, TickPriority ,0U);
   
-//   /* Enable the TIM6 global Interrupt */
-//   HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
+  /* Enable the TIM6 global Interrupt */
+  HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
   
-//   /* Enable TIM6 clock */
-//   __HAL_RCC_TIM6_CLK_ENABLE();
+  /* Enable TIM6 clock */
+  __HAL_RCC_TIM6_CLK_ENABLE();
   
-//   /* Get clock configuration */
-//   HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
+  /* Get clock configuration */
+  HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
   
-//   /* Get APB1 prescaler */
-//   uwAPB1Prescaler = clkconfig.APB1CLKDivider;
+  /* Get APB1 prescaler */
+  uwAPB1Prescaler = clkconfig.APB1CLKDivider;
   
-//   /* Compute TIM6 clock */
-//   if (uwAPB1Prescaler == RCC_HCLK_DIV1) 
-//   {
-//     uwTimclock = HAL_RCC_GetPCLK1Freq();
-//   }
-//   else
-//   {
-//     uwTimclock = 2U*HAL_RCC_GetPCLK1Freq();
-//   }
+  /* Compute TIM6 clock */
+  if (uwAPB1Prescaler == RCC_HCLK_DIV1) 
+  {
+    uwTimclock = HAL_RCC_GetPCLK1Freq();
+  }
+  else
+  {
+    uwTimclock = 2U*HAL_RCC_GetPCLK1Freq();
+  }
   
-//    Compute the prescaler value to have TIM6 counter clock equal to 1MHz 
-//   uwPrescalerValue = (uint32_t) ((uwTimclock / 1000000U) - 1U);
+   // Compute the prescaler value to have TIM6 counter clock equal to 1MHz 
+  uwPrescalerValue = (uint32_t) ((uwTimclock / 1000000U) - 1U);
   
-//   /* Initialize TIM6 */
-//   TimHandle.Instance = TIM6;
+  /* Initialize TIM6 */
+  TimHandle.Instance = TIM6;
   
-//   /* Initialize TIMx peripheral as follow:
-//   + Period = [(TIM6CLK/1000U) - 1]. to have a (1U/1000U) s time base.
-//   + Prescaler = (uwTimclock/1000000U - 1U) to have a 1MHz counter clock.
-//   + ClockDivision = 0
-//   + Counter direction = Up
-//   */
-//   TimHandle.Init.Period = (1000000U / 1000U) - 1U;
-//   TimHandle.Init.Prescaler = uwPrescalerValue;
-//   TimHandle.Init.ClockDivision = 0U;
-//   TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
-//   TimHandle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-//   if(HAL_TIM_Base_Init(&TimHandle) == HAL_OK)
-//   {
-//     /* Start the TIM time Base generation in interrupt mode */
-//     return HAL_TIM_Base_Start_IT(&TimHandle);
-//   }
+  /* Initialize TIMx peripheral as follow:
+  + Period = [(TIM6CLK/1000U) - 1]. to have a (1U/1000U) s time base.
+  + Prescaler = (uwTimclock/1000000U - 1U) to have a 1MHz counter clock.
+  + ClockDivision = 0
+  + Counter direction = Up
+  */
+  TimHandle.Init.Period = (1000000U / 1000U) - 1U;
+  TimHandle.Init.Prescaler = uwPrescalerValue;
+  TimHandle.Init.ClockDivision = 0U;
+  TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
+  TimHandle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if(HAL_TIM_Base_Init(&TimHandle) == HAL_OK)
+  {
+    /* Start the TIM time Base generation in interrupt mode */
+    return HAL_TIM_Base_Start_IT(&TimHandle);
+  }
   
-//   /* Return function status */
-//   return HAL_ERROR;
-// }
+  /* Return function status */
+  return HAL_ERROR;
+}
 
 /**
   * @brief  Suspend Tick increment.
--- build_tools/arctic_awesomeness/interrupt_demo/interrupt_demo.c
+++ build_tools/arctic_awesomeness/interrupt_demo/interrupt_demo.c
@@ -16,7 +16,7 @@
 
 static void cpu_periodic_task_init(void)
 {
-  SysTick->LOAD  = (uint32_t)8000000;                         /* set reload register */
+  SysTick->LOAD  = (uint32_t)8000000;                               /* set reload register */
   NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL); /* set Priority for Systick Interrupt */
   SysTick->VAL   = 0UL;                                             /* Load the SysTick Counter Value */
   SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
