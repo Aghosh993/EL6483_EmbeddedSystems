731489c78d0a882b99ebc5a27f8335ad173053dd
Aghosh993 <Aghosh993@gmail.com>
2017-03-11 04:44:27 -0500

    Asm blink is actually a blink program now. Abhi.

--- build_tools/arctic_awesomeness/asm_blinky/asm_blinky.s
+++ build_tools/arctic_awesomeness/asm_blinky/asm_blinky.s
@@ -14,8 +14,11 @@
 .equ GPIO_MODE, 0x48001000
 .equ GPIO_OTYPE, 0x48001004
 .equ GPIO_OSPEED, 0x48001008
+.equ GPIO_ODR, 0x48001014
 .equ GPIO_PUPD, 0x4800100C
 .equ GPIO_BSRR, 0x48001018
+
+.equ DELAY_RELOAD_VAL, 0xFF00
 
 /* 
 	We tell the assembler/linker that this code goes in the "text"
@@ -28,6 +31,7 @@
 	.syntax unified
 
 main:
+	cpsid i
 	ldr R0, =RCC_AHB_reg
 	ldr R1, [R0]
 	orr R1, R1, #(0x01 << 21)
@@ -35,13 +39,25 @@ main:
 
 	ldr R0, =GPIO_MODE
 	ldr R1, [R0]
-	orr R1, R1, #(0x01 << 16)
+	orr R1, R1, #(0x01 << 18)
 	str R1, [R0]
 
-	ldr R0, =GPIO_BSRR
+	ldr R0, =GPIO_ODR
 	ldr R1, [R0]
-	orr R1, R1, #(0x01 << 8)
+	orr R1, R1, #(0x01 << 9)
 	str R1, [R0]
 
 SimpleInfLoop:
+	mov R1, DELAY_RELOAD_VAL
+Delay1:
+	sub R1, R1, #0x1
+	cbnz R1, jmp_Delay1
+	b TogglePin
+jmp_Delay1:
+	b Delay1
+TogglePin:
+	ldr R0, =GPIO_ODR
+	ldr R1, [R0]
+	eor R1, R1, #(0x01 << 9)
+	str R1, [R0]
 	b SimpleInfLoop
